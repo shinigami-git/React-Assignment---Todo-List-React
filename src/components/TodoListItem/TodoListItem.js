c79968632bc5787a97a4acfc18eac60b900985d6
shinigami-git <nitink.kmr638@gmail.com>
2021-02-23 19:12:51 +0530

    save button disbaled for empty string

--- src/components/TodoListItem/TodoListItem.js
+++ src/components/TodoListItem/TodoListItem.js
@@ -1,54 +1,76 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./style.css";
 
-const TodoListItem = ({ listItem,index,todoListValues,setTodoListValues }) => {
+const TodoListItem = ({
+  listItem,
+  index,
+  todoListValues,
+  setTodoListValues,
+}) => {
   const [isEditing, setIsEditing] = useState(false);
   const [currentText, setCurrentText] = useState(listItem);
-  const handleEdit=()=>{
-      setIsEditing(true);
-  }
+  const handleEdit = () => {
+    setIsEditing(true);
+  };
+  const saveButton = React.createRef();
 
-  const handleDelete=()=>{
-      let clonedArr=[...todoListValues];
-      clonedArr.splice(index,1);
-      setTodoListValues(clonedArr);
-  }
-
-  const changeText=(event)=>{
-        setCurrentText(event.target.value);
-  }
-
-  const handleSave=()=>{
-    
-    console.log(currentText.replace(/\s/g,''));  
-    if(currentText.replace(/\s/g,'')!==""){
-    let clonedArr=[...todoListValues];
-    clonedArr[index]=currentText;
+  const handleDelete = () => {
+    let clonedArr = [...todoListValues];
+    clonedArr.splice(index, 1);
     setTodoListValues(clonedArr);
-    setIsEditing(false);    
-}
-    
-  }
+  };
 
+  const changeText = (event) => {
+    setCurrentText(event.target.value);
+  };
+
+  const handleSave = () => {
+    // console.log(currentText.replace(/\s/g,''));
+    if (currentText.replace(/\s/g, "") !== "") {
+      let clonedArr = [...todoListValues];
+      clonedArr[index] = currentText;
+      setTodoListValues(clonedArr);
+      setIsEditing(false);
+    }
+  };
+  useEffect(() => {
+    if (saveButton.current !== null) {
+      if (currentText.replace(/\s/g, "") == "") {
+        saveButton.current.disabled = true;
+      } else {
+        saveButton.current.disabled = false;
+      }
+    }
+  }, [currentText]);
 
   if (isEditing) {
     return (
-      <div className="added-task" >
-        <textarea className="editTask" onChange={changeText} defaultValue={listItem}></textarea>
+      <div className="added-task">
+        <textarea
+          className="editTask"
+          onChange={changeText}
+          defaultValue={listItem}
+        ></textarea>
         <br />
-        <button className="saveTask" onClick={handleSave}>Save</button>
+        <button className="saveTask" onClick={handleSave} ref={saveButton}>
+          Save
+        </button>
       </div>
     );
   } else {
-    return <div className="added-task" >
-        <div className="todo-items">
-        {listItem}
-        </div>
+    return (
+      <div className="added-task">
+        <div className="todo-items">{listItem}</div>
         <div className="buttons">
-        <button className="edit" onClick={handleEdit}>Edit</button>
-        <button className="delete" onClick={handleDelete} >Delete</button>
+          <button className="edit" onClick={handleEdit}>
+            Edit
+          </button>
+          <button className="delete" onClick={handleDelete}>
+            Delete
+          </button>
         </div>
-        </div>;
+      </div>
+    );
   }
 };
 
